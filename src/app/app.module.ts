b7a35b9da5a89b2f038ed746bff35f09eb15fa4e
Afrosemobeen <afrosemobeenmohammad@gmail.com>
2022-10-02 19:32:56 +0530

    Changes to be committed:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
+import { TestComponent } from './components/test/test.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
